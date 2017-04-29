bd4be49ec69403e5ade86272a56c15fc5b581f05
curio <curio@DESKTOP-601CS4L.lan>
2017-04-29 00:49:14 -0700

    added doc strings to Movie

--- media.py
+++ media.py
@@ -1,7 +1,23 @@
-class Movie():
+class Movie(object):
+    
+    """Simple container of movie information.
+    
+    Attributes:
+        title: title of the movie
+        poster_image_url: absolute http url to an image of the movie's poster
+        trailer_youtube_url: absolute http url to a trailer for the movie
+    """
     
     
     def __init__(self, title, poster_image_url, trailer_youtube_url):
+        
+        """Construct movie structure.
+        
+        Args:
+            title: title of the movie
+            poster_image_url: absolute http url to an image of the movie's poster
+            trailer_youtube_url: absolute http url to a trailer for the movie
+        """
         
         self.title = title
         self.poster_image_url = poster_image_url
